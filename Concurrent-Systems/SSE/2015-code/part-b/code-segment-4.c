ae4010be7cae85d579d096231f1fcf33a918d4a0
alexandersep <sepelena@tcd.ie>
2023-04-01 17:01:21 +0100

    This change is still a broken version of 2015

--- Concurrent-Systems/SSE/2015-code/part-b/code-segment-4.c
+++ Concurrent-Systems/SSE/2015-code/part-b/code-segment-4.c
@@ -2,39 +2,50 @@
 #include <stdlib.h>
 #include <immintrin.h>
 
-// TODO: This is not the correct function
-
-// It is the skieloton that I have yet to add
-// find maximum of array
-float find_max(float* array, int size) {
-    __m128 v4max = _mm_load1_ps(array); // load first four values
-    const int size_minus_three = size - 3;
-    int i;
-    for (i = 1; i < size_minus_three; i+=4) {
-        __m128 v4array = _mm_loadu_ps(array+i);
-        v4max = _mm_max_ps(v4array, v4max); 
+int max_index(float* array) {
+    __m128 v4max = _mm_load1_ps(array);
+    __m128 v4max_idx = _mm_setzero_ps();
+    int max_idx = 0;
+    for (int i = 0; i < 4096; i+=4) { // start from 0, to avoid potential undefined lane
+        __m128 v4a = _mm_loadu_ps(array+i);
+        __m128 v4a_cmpgt_v4max = _mm_cmpgt_ps(v4a, v4max);
+        __m128 v4i = _mm_set_ps(i+3, i+2, i+1, i);
+        v4max_idx = _mm_max_ps(v4max_idx, v4a);
+        v4max_idx = _mm_blend_ps(v4max_idx, v4a_idx, v4a_cmpgt_v4max);
     }
-    v4max = _mm_max_ps(v4max, _mm_shuffle_ps(v4max, v4max, _MM_SHUFFLE(0,0,3,2)));
-    v4max = _mm_max_ps(v4max, _mm_shuffle_ps(v4max, v4max, _MM_SHUFFLE(0,0,0,1)));
-    float max = _mm_cvtss_f32(v4max);
-    // i = i;
-    for (; i < size; i++) {
-        if (array[i] > max) { 
-            max = array[i]; 
+    int result = 0;
+    float max = _mm_cvtss_f32(max_val);  // extract the maximum value from max_val
+    float* max_idx_ptr = (float*)&v4max_idx;  // cast max_idx to an int pointer
+    for (int i = 0; i < 4; i++) {
+        if (array[max_idx_ptr[i]] == max) {  // check which of the four indices in max_idx_ptr corresponds to the maximum value
+            result = (int) max_idx_ptr[i];
+            break;
         }
     }
-    return max;
+    return result;
 }
 
+/*
+int max_index(float* array) {
+    float max = array[0];
+    int max_idx = 0;
+    for (int i = 1; i < 4096; i++) {
+        if (array[i] > max) {
+            max_idx = i;
+        }
+    }
+    return max_idx;
+}
+*/
+
 int main() {
-    const int SIZE = 1025; 
-    const float multiplier = 1.5f;
+    const int SIZE = 4096; 
     float* array = malloc(sizeof(float) * SIZE); 
     // set values for testing
     for (int i = 0; i < SIZE; i++) {
         array[i] = i;
     }
-    printf("max = %f\n", find_max(array,SIZE));
+    printf("max = %f\n", max_index(array));
     free(array);
     return 0;
 }
