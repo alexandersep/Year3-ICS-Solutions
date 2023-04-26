da085491114dc80901d489acbae1ef5024dc4ea6
alexandersep <sepelena@tcd.ie>
2023-04-26 22:23:52 +0100

    Fix comment for routine 6 and add adjective for robert

--- Concurrent-Systems/Sample-Exam-2023/Question-1/csu33014-annual-q1-code.c
+++ Concurrent-Systems/Sample-Exam-2023/Question-1/csu33014-annual-q1-code.c
@@ -210,7 +210,7 @@ void Q1_vectorized_4(float * restrict a, float * restrict b, float * restrict c)
 // t0x helped with getting me a solution with 6 loads and looping 4 at a time
 // it was then improved by me (Alexander) with the loop unrolling if statement
 // and cache prediction with moving a[2047] at the end.
-// It was then later improved by Robert Gaynor, IG handle: rogob101 regarding
+// It was then later improved by The spectacular Robert Gaynor, IG handle: rogob101 regarding
 // optimising two loads and using shuffles
 void Q1_vectorized_4(float * restrict a, float * restrict b, float * restrict c) {
     a[0] = b[0] * c[0];
@@ -222,7 +222,7 @@ void Q1_vectorized_4(float * restrict a, float * restrict b, float * restrict c)
         __m128 vf32_b = _mm_shuffle_ps(vf32_b_minus, vf32_b_plus, _MM_SHUFFLE(2, 1, 2, 1)); // optimise load instruction be reusing b[i-1] b[i+1] values
         __m128 vf32_c_minus = _mm_loadu_ps(&c[i-1]);        
         __m128 vf32_c_plus = _mm_loadu_ps(&c[i+1]);        
-        __m128 vf32_c = _mm_shuffle_ps(vf32_c_minus, vf32_c_plus, _MM_SHUFFLE(2, 1, 2, 1)); // optimise load instruction be reusing b[i-1] b[i+1] values
+        __m128 vf32_c = _mm_shuffle_ps(vf32_c_minus, vf32_c_plus, _MM_SHUFFLE(2, 1, 2, 1)); // optimise load instruction be reusing c[i-1] c[i+1] values
         
         __m128 vf32_mul_minus = _mm_mul_ps(vf32_b_minus,vf32_c_plus);
         __m128 vf32_mul = _mm_mul_ps(vf32_b, vf32_c);
