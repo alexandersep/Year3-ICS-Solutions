c963144396fcd1eb6df48202d9917edd9f709e97
alexandersep <sepelena@tcd.ie>
2023-04-27 23:02:43 +0100

    Optimised code segment 3

--- Concurrent-Systems/SSE/2009-code/part-b/code-segment-3.c
+++ Concurrent-Systems/SSE/2009-code/part-b/code-segment-3.c
@@ -9,7 +9,8 @@ void code_segment_3(float * vals) {
 
 /* array "vals" is 16-byte aligned */
 void vec_code_segment_3(float * vals) {
-    __m128 vf32_vals = _mm_set1_ps(vals[0]);
+    __m128 vf32_vals = _mm_loadu_ps(&vals[0]); // [1, 2, 3, 4]
+    vf32_vals = _mm_shuffle_ps(vf32_vals, vf32_vals, _MM_SHUFFLE(0,0,0,0)); // optimise out set_ps
     for (int i = 0; i < 1024; i+=4) {
         _mm_store_ps(&vals[i], vf32_vals);
     }
